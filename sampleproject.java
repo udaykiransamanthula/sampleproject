380f620fd208d2521ca04c24ab2ddc612d81a20d
Ubuntu <ubuntu@ip-10-0-1-211.ap-south-1.compute.internal>
2020-04-17 10:59:17 +0000

    added by uday

---
+++ sampleproject.java
@@ -0,0 +1,8 @@
+class sampleproject {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    { int i; for(i=0;i<=10;i++)
+        System.out.println("Hello, my sampleproject");
+    } 
+} 
